24a44f3adcdcd90c784aca20fa83f1ab48aba990
huris <huris102234@gmail.com>
2018-12-14 16:18:47 +0800

    将原来Utility.java类中json解析方式改为gson

--- app/src/main/java/com/huris/simpleweather/util/Utility.java
+++ app/src/main/java/com/huris/simpleweather/util/Utility.java
@@ -1,7 +1,10 @@
 package com.huris.simpleweather.util;
 
 import android.text.TextUtils;
+import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.huris.simpleweather.db.City;
 import com.huris.simpleweather.db.County;
 import com.huris.simpleweather.db.Province;
@@ -10,6 +13,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 // 由于服务器返回的省市县数据都是JSON格式的,所以我们最好再提供一个工具类来解析和处理这种数据
 // 在util包下新建一个Utility类
 public class Utility {
@@ -17,21 +22,20 @@ public class Utility {
      * 解析和处理服务器返回的省级数据
      */
     public static boolean handleProvinceResponse(String response) {
-        if(!TextUtils.isEmpty(response)) {
+        if (!TextUtils.isEmpty(response)) {
             try {
-                // 先使用JSONArray和JSONObject将数据解析出来
-                JSONArray allProvinces = new JSONArray(response);
-                for(int i = 0; i < allProvinces.length(); i++) {
-                    // 组装成实体类对象
-                    JSONObject provinceObject = allProvinces.getJSONObject(i);
-                    Province province = new Province();
-                    province.setProvinceName(provinceObject.getString("name"));
-                    province.setProvinceCode(provinceObject.getInt("id"));
-                    // 调用save()方法将数据存储到数据库中
+                // 使用gson解析获得的response
+                Gson gson = new Gson();
+                // 生成所有省的列表
+                List<Province> allProvinces = gson.fromJson(response,
+                        new TypeToken<List<Province>>() {
+                        }.getType());
+                for (Province province : allProvinces) {
+                    // 将数据保存到数据库中
                     province.save();
                 }
                 return true;
-            } catch (JSONException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -42,20 +46,21 @@ public class Utility {
      * 解析和处理服务器返回的市级数据
      */
     public static boolean handleCityResponse(String response, int provinceId) {
-        if(!TextUtils.isEmpty(response)) {
+        if (!TextUtils.isEmpty(response)) {
             try {
-                JSONArray allCities = new JSONArray(response);
-                for(int i = 0; i < allCities.length(); i++) {
-                    JSONObject cityObject = allCities.getJSONObject(i);
-                    City city = new City();
-                    city.setCityName(cityObject.getString("name"));
-                    city.setCityCode(cityObject.getInt("id"));
-                    city.setProvinceId(provinceId);
+                // 使用gson解析获得的response
+                Gson gson = new Gson();
+                // 生成所有市的列表
+                List<City> allCities = gson.fromJson(response,
+                        new TypeToken<List<City>>() {
+                        }.getType());
+                for (City city : allCities) {
+                    // 将数据保存到数据库中
                     city.save();
                 }
                 return true;
-            }catch (JSONException e) {
-                e.printStackTrace();;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         return false;
@@ -65,26 +70,23 @@ public class Utility {
      * 解析和处理服务器返回的县级数据
      */
     public static boolean handleCountyResponse(String response, int cityId) {
-        if(!TextUtils.isEmpty(response)) {
+        if (!TextUtils.isEmpty(response)) {
             try {
-                JSONArray allCounties = new JSONArray(response);
-                for(int i = 0; i < allCounties.length(); i++) {
-                    JSONObject countyObject = allCounties.getJSONObject(i);
-                    County county = new County();
-                    county.setCountyName(countyObject.getString("name"));
-                    county.setWeatherId(countyObject.getString("weather_id"));
-                    county.setCityId(cityId);
+                // 使用gson解析获得的response
+                Gson gson = new Gson();
+                // 生成所有县的列表
+                List<County> allCounties = gson.fromJson(response,
+                        new TypeToken<List<County>>() {
+                        }.getType());
+                for (County county : allCounties) {
+                    // 将数据保存到数据库中
                     county.save();
                 }
                 return true;
-            }catch (JSONException e) {
-                e.printStackTrace();;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         return false;
     }
-
-
-
-
 }
