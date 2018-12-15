7ce7494904e913b608836017237d4284239d9a9c
huris <huris102234@gmail.com>
2018-12-15 23:10:15 +0800

    完成正常显示功能

--- app/src/main/java/com/huris/simpleweather/MainActivity.java
+++ app/src/main/java/com/huris/simpleweather/MainActivity.java
@@ -1,5 +1,8 @@
 package com.huris.simpleweather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // onCreat()方法一开始先从SharedPreferences文件中读取缓存数据
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            // 如果不为null就说明之前已经请求过天气数据了,那么就没必要让用户再次选择城市,而是直接跳转到WeatherActivity即可
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
