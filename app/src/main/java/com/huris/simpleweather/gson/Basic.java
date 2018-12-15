3a611540017e84b9ab6a30b12c4e0e3cf55ee85a
huris <huris102234@gmail.com>
2018-12-15 19:31:44 +0800

    添加gson包中的Basic类

---
+++ app/src/main/java/com/huris/simpleweather/gson/Basic.java
@@ -0,0 +1,36 @@
+package com.huris.simpleweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+
+    // 由于JSON中的一些字段可能不太适合作为java的字段来命名
+    // 因此这里使用了@SerializedName注解的方式来让JSON字段和JAVA之间建立映射关系
+    /**
+     * 城市名
+     */
+    @SerializedName("city")
+    public String cityName;
+
+    /**
+     * 城市id
+     */
+    @SerializedName("id")
+    public String weatherId;
+
+    /**
+     * 更新时间
+     */
+    public Update update;
+
+    /**
+     * 内部类
+     */
+    public class Update {
+
+        @SerializedName("loc")
+        public String updateTime;
+
+    }
+
+}
