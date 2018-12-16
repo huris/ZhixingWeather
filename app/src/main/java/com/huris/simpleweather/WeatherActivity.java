b123fcf7c9497a880f7b988f48718af978952493
huris <huris102234@gmail.com>
2018-12-16 23:24:16 +0800

    布局完毕

--- app/src/main/java/com/huris/simpleweather/WeatherActivity.java
+++ app/src/main/java/com/huris/simpleweather/WeatherActivity.java
@@ -266,13 +266,14 @@ public class WeatherActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     StringBuilder currentPosition = new StringBuilder();
+                    currentPosition.append("经度：").append(location.getLongitude()).append("   ");
                     currentPosition.append("纬度：").append(location.getLatitude()).append("\n");
-                    currentPosition.append("经线：").append(location.getLongitude()).append("\n");
-                    currentPosition.append("国家：").append(location.getCountry()).append("\n");
-                    currentPosition.append("省：").append(location.getProvince()).append("\n");
-                    currentPosition.append("市：").append(location.getCity()).append("\n");
-                    currentPosition.append("区：").append(location.getDistrict()).append("\n");
-                    currentPosition.append("街道：").append(location.getStreet()).append("\n");
+                    currentPosition.append("地址：").append(location.getAddrStr()).append("\n");
+//                    currentPosition.append("国家：").append(location.getCountry()).append("   ");
+//                    currentPosition.append("省份：").append(location.getProvince()).append("\n");
+//                    currentPosition.append("市：").append(location.getCity()).append("   ");
+//                    currentPosition.append("区：").append(location.getDistrict()).append("\n");
+//                    currentPosition.append("街道：").append(location.getBuildingName()).append("  ");
                     currentPosition.append("定位方式：");
                     if (location.getLocType() == BDLocation.TypeGpsLocation) {
                         currentPosition.append("GPS");
