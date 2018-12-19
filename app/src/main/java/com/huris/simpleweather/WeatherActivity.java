7bf37923d05da6a683372cf307b08373d70b2e99
huris <huris102234@gmail.com>
2018-12-19 15:54:25 +0800

    修改地址前的位置

--- app/src/main/java/com/huris/simpleweather/WeatherActivity.java
+++ app/src/main/java/com/huris/simpleweather/WeatherActivity.java
@@ -16,6 +16,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -114,9 +115,12 @@ public class WeatherActivity extends AppCompatActivity {
 //            // 再调用它的setSystemUiVisibility()方法来改变系统UI的显示
 //            // View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN和
 //            // View.SYSTEM_UI_FLAG_LAYOUT_STABLE表示活动的布局会显示在状态栏上面
-//            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+//            decorView.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 //            // 调用setStatusBarColor()方法将状态栏设置成透明色
+//            getWindow().setNavigationBarColor(Color.TRANSPARENT);
 //            getWindow().setStatusBarColor(Color.TRANSPARENT);
 //        }
 
@@ -172,9 +176,9 @@ public class WeatherActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setLogo(R.mipmap.logo);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
+//        getSupportActionBar().setLogo(R.mipmap.logo);
 
         // 当按钮被按下时,切换到地图视图
         mapPosition.setOnClickListener(new View.OnClickListener() {
